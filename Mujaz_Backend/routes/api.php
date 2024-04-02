6809a4e08689d199debf751ec7c5c17bae3fc641
Yaman-Th <Tahhan152@gmail.com>
2024-04-03 02:30:34 +0300

    Update api.php
    Add routes for teacher and student

--- Mujaz_Backend/routes/api.php
+++ Mujaz_Backend/routes/api.php
@@ -58,7 +58,6 @@ Route::group((['prefix' => 'admin', 'middleware' => ['auth:sanctum']]), function
     // GetListOfTeachers
     Route::get('/teachers', [TeacherController::class, 'index']);
 
-
     // AddNewSession
     Route::post('/session/add', [SessionController::class, 'store']);
 
@@ -85,10 +84,61 @@ Route::group((['prefix' => 'admin', 'middleware' => ['auth:sanctum']]), function
 // RoutesForTeacher
 Route::group((['prefix' => 'teacher', 'middleware' => ['auth:sanctum']]), function () {
 
+    // Logout
+    Route::post('/user/logout', [UserController::class, 'logout']);
+
     // AddNewUser
     Route::post('/user/add', [UserController::class, 'store']);
+
+    // DeleteUser
+    Route::delete('/user/destroy/{user}', [UserController::class, 'destroy']);
+
+    // UpdateUserInformtion
+    Route::put('/user/{user}', [UserController::class, 'update']);
+
+    // GetStudentInfo
+    Route::get('/student/info/{student}', [StudentController::class, 'showInfo']);
+
+    // GetStudentsByTeacher
+    Route::get('/students/{teacher}', [StudentController::class, 'getByTeacher']);
+
+    // FillTeacherInformation
+    Route::put('/teacher/form/{teacher}', [TeacherController::class, 'update']);
+
+    // AddNewSession
+    Route::post('/session/add', [SessionController::class, 'store']);
+
+    // UpdateSession
+    Route::put('/session/{session}', [SessionController::class, 'update']);
+
+    // DestroySession
+    Route::delete('/session/destroy/{session}', [SessionController::class, 'destroy']);
+
+    // GetSessionsByTeacher
+    Route::get('/session/teachers/{teacher}', [SessionController::class, 'getByTeacher']);
+
+    // GetSessionsByFilter
+    Route::get('/session', [SessionController::class, 'filteredSessions']);
 });
 
 // RoutesForStudents
 Route::group((['prefix' => 'student', 'middleware' => ['auth:sanctum']]), function () {
+
+    // UpdateUserInformtion
+    Route::put('/user/{user}', [UserController::class, 'update']);
+
+    // Logout
+    Route::post('/user/logout', [UserController::class, 'logout']);
+
+    // FillStudentInfo
+    Route::put('/student/form/{student}', [StudentController::class, 'update']);
+
+    // GetStudentInfo
+    Route::get('/student/info/{student}', [StudentController::class, 'showInfo']);
+
+    // GetSessionsByStudent
+    Route::get('/session/students/{student}', [SessionController::class, 'getByStudent']);
+
+    // GetSessionsByFilter
+    Route::get('/session', [SessionController::class, 'filteredSessions']);
 });
